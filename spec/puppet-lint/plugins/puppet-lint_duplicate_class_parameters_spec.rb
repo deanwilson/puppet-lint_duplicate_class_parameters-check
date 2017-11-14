9191a65684fe453dbb0406caa97f8219c0505923
Dean Wilson <dean.wilson@gmail.com>
2017-11-14 19:18:40 +0000

    Handle classes with types and duplicated right hand sides.
    The param parsing code was a little naive before and didn't
    take in to account that you can have the same value on the
    right hand side of an assignment. This commit adds specs to
    test for this and code to make it work.
    
    Also add an example that uses types to ensure I don't cause a
    regression.

--- spec/puppet-lint/plugins/puppet-lint_duplicate_class_parameters_spec.rb
+++ spec/puppet-lint/plugins/puppet-lint_duplicate_class_parameters_spec.rb
@@ -39,6 +39,46 @@ describe 'duplicate_class_parameters' do
     end
   end
 
+  context 'class with duplicate right hand side' do
+    let(:code) do
+      <<-EOS
+        class duplicated_rhs(
+          $nagios_innodb_enable = $sys11mysql,
+          $nagios_myisam_enable = $sys11mysql,
+        ) {
+          file { '/tmp/my-file':
+            mode => '0600',
+          }
+        }
+      EOS
+    end
+
+    it 'should not detect any problems' do
+      expect(problems).to have(0).problems
+    end
+  end
+
+  context 'class with type and duplicate right hand side' do
+    let(:code) do
+      <<-EOS
+        class duplicated_rhs(
+          Boolean $nagios_innodb_enable = $sys11mysql::params::production_environment,
+          Boolean $nagios_myisam_enable = $sys11mysql::params::production_environment,
+        ) {
+          file { '/tmp/my-file':
+            mode => '0600',
+          }
+        }
+      EOS
+    end
+
+    it 'should not detect any problems' do
+      expect(problems).to have(0).problems
+    end
+  end
+
+  # Examples that should fail specs
+
   context 'class with duplicate parameters' do
     let(:msg) { 'found duplicate parameter \'duplicated\' in class \'file_resource\'' }
     let(:code) do
@@ -74,4 +114,32 @@ describe 'duplicate_class_parameters' do
     end
 
   end
+
+  context 'class with type and duplicate parameters' do
+    let(:msg) { 'found duplicate parameter \'not_unique\' in class \'duplicated_type\'' }
+    let(:code) do
+      <<-EOS
+        class duplicated_type(
+          Boolean $not_unique = true,
+          Boolean $not_unique = false,
+        ) {
+
+          file { '/tmp/my-file':
+            mode => '0600',
+          }
+
+          $baz = 'xxzzy'
+        }
+      EOS
+    end
+
+    it 'should detect two problems' do
+      expect(problems).to have(1).problems
+    end
+
+    it 'should create a warning' do
+      expect(problems).to contain_warning(msg).on_line(3).in_column(19)
+    end
+  end
+
 end
