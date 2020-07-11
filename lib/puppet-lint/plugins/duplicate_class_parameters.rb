28617587b16824f884cd91a0a5c6e4d1cce440e8
Dean Wilson <dean.wilson@gmail.com>
2020-07-11 08:45:47 +0100

    Replace an if with an early next and remove an indentation level
    8 rubocops to go!

--- lib/puppet-lint/plugins/duplicate_class_parameters.rb
+++ lib/puppet-lint/plugins/duplicate_class_parameters.rb
@@ -19,36 +19,35 @@ PuppetLint.new_check(:duplicate_class_parameters) do
         end
 
         next unless level.zero?
+        next unless token.type == :VARIABLE
 
-        if token.type == :VARIABLE
-          next_type = token.next_code_token.type
+        next_type = token.next_code_token.type
 
-          # handling for lines with an equals and a variable on the rhs
-          if next_type == :EQUALS
-            inside = true
+        # handling for lines with an equals and a variable on the rhs
+        if next_type == :EQUALS
+          inside = true
 
-            two_away = token.next_code_token.next_code_token.type
-            if two_away == :DQPRE
-              inside = false
-            end
-          elsif next_type == :COMMA
+          two_away = token.next_code_token.next_code_token.type
+          if two_away == :DQPRE
             inside = false
           end
+        elsif next_type == :COMMA
+          inside = false
+        end
 
-          # only proceed if we're at the end of a declaration.
-          next if !inside
+        # only proceed if we're at the end of a declaration.
+        next unless inside
 
-          param_name = token.value
-          seen[param_name] += 1
+        param_name = token.value
+        seen[param_name] += 1
 
-          if seen[param_name] > 1
-            # warning output shows the parameter location each additional time it's seen
-            notify :warning, {
-              :message => "found duplicate parameter '#{param_name}' in class '#{class_name}'",
-              :line    => token.line,
-              :column  => token.column,
-            }
-          end
+        if seen[param_name] > 1
+          # warning output shows the parameter location each additional time it's seen
+          notify :warning, {
+            message: "found duplicate parameter '#{param_name}' in class '#{class_name}'",
+            line:    token.line,
+            column:  token.column,
+          }
         end
       end
     end
